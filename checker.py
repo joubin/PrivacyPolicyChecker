a82df91237bc36ecb4a819873ae0a68b9025fbf9
Joubin Jabbari <joubin.j@gmail.com>
2012-11-21 22:53:07 -0800

    Code Cleanup
    Removed un needed comments for better code reading

--- checker.py
+++ checker.py
@@ -1,31 +1,19 @@
-
-# The following code simply takes an input and downloads the privacy policy
-# Usage: python checker.py amazon privacy policy
-# will produce a file called amazon%20privacy%20policy
-# if such file exists, it will create the above with a .tmp extention
-# Joubin Jabbari | Mitch Brennan | Alex Chernyak 
 import datetime
-#import _mysql
 import urllib
 import hashlib
-#import urllib2
 import sys
 import requests
-from readability.readability import Document
 import os.path
 import datetime
 import os
-######################################################################################
-######################################################################################
-######################################################################################
+from readability.readability import Document
+
+
 def checkerFunction(myInput):
 	today = datetime.date.today()
-	#print today
-	# the commandline input
 	
 	
-	# Google the input and use the first likes location
-	# This is the first result google gets 
+
 
 
 
@@ -34,21 +22,15 @@ def checkerFunction(myInput):
 		google2 = '%20privacy%20policy&btnI=1'
 		keyword = myInput
 		
-		# Reconstruct the URL
 		url = google1 + keyword + google2
-		#Send URL Request 
 		r = requests.get(url, allow_redirects=False)
 		url = r.headers['location']
 	except Exception as e:
 		return
 
-	# Print URL for testing (Should be commented out during implementation as it will create noise)
-	#print url
+
 	
-	#Store the location of the file
 	myFullPath = "./sandbox/db/" + keyword
-	## ./sandbox/db/amazon/amazon.ddate
-	## ./sandbox/db/amazon/amazon.ddate.tmp
 
 	if not os.path.exists("./sandbox"):
     	  os.makedirs("./sandbox")
@@ -63,13 +45,10 @@ def checkerFunction(myInput):
 	filetowrite = myFullPath + "/" + filename
 	
 	fileExist =  os.path.isfile(filetowrite)
-	#print "filetowrite is %s" % filetowrite
 	
 	
 	
-	#if not os.path.exists(myFullPath): os.makedirs(myFullPath)
 	
-	# Create a human readable document. Strips out most but NOT all of the html tags
 	if (url == None):
 		return
 	html = urllib.urlopen(url).read()
@@ -77,7 +56,6 @@ def checkerFunction(myInput):
 	tempFileMade = False
 	originalFileMade = False
 	if(fileExist):
-		#print "its there, ill make a temp"
 		filetowrite = filetowrite + ".tmp."
 		f = open(filetowrite, 'w')
 		writeThis = str(readable_article.encode('ascii', 'ignore')) 
@@ -85,7 +63,6 @@ def checkerFunction(myInput):
 		f.close
 		tempFileMade = True
 	else:
-		#print "no, its not there, ill go ahead and make it"
 		f = open(filetowrite, 'w')
 		writeThis = str(readable_article.encode('ascii', 'ignore'))
 		f.write(writeThis)
@@ -94,5 +71,4 @@ def checkerFunction(myInput):
 	
 	hashedmd5 = hashlib.md5(readable_article.encode('ascii', 'ignore'))
 	hashedArticle = hashedmd5.hexdigest()
-	#print "%s" % hashedArticle
 	return hashedArticle	
